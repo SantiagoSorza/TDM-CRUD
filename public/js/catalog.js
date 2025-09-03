89839d3de7fd8de8c85e79a435903fe7aa026a2b
santiagosorza <santiagosorzag@gmail.com>
2025-09-03 08:38:37 -0500

    index nice

--- public/js/catalog.js
+++ public/js/catalog.js
@@ -10,8 +10,9 @@ function renderItem(item) {
     document.getElementById("pro-id").textContent = `ID: ${item.id || "-"}`;
     document.getElementById("pro-descrip").textContent = item.description || "Sin descripción";
     document.getElementById("pro-value").textContent = `Precio: ${item.price || "-"}`;
-    document.getElementById("pro-date").textContent = `Fecha: ${item.creationDate || "-"}`;
+    document.getElementById("pro-date").textContent = `Fecha: ${item.date || "-"}`;
     document.getElementById("pro-categorie").textContent = `Categoría: ${item.category || "-"}`;
+    
 }
 // Función principal para cargar los items desde la API
 async function loadCatalog() {
